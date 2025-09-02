b111097dcf91ec13c8d55a37b665320033acb9c0
meetpatoliya001 <meetpatoliya001@gmail.com>
2025-09-02 20:15:32 +0530

    fronted and backend setup

---
+++ backend/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+const allowedOrigins = ["http://localhost:5173"];
+
+app.use(cors());
+
+
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     // Allow requests with no origin (like Postman blocked)
+//     if (!origin) return callback(new Error("Blocked: No origin"), false);
+
+//     if (allowedOrigins.includes(origin)) {
+//       callback(null, true);  // Allow frontend
+//     } else {
+//       callback(new Error("Blocked by CORS"), false); // Block others
+//     }
+//   },
+//   credentials: true
+// }));
+
+// app.use((req, res, next) => {
+//   const key = req.headers["x-api-key"];
+//   if (key === process.env.FRONTEND_SECRET_KEY) {
+//     next();
+//   } else {
+//     res.status(403).json({ error: "Forbidden" });
+//   }
+// });
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+const mongodb_url = process.env.MONGO_URL;
+
+mongoose.connect(mongodb_url).then(() => {
+  console.log("Connected to MongoDB!");
+  app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
+})
+  .catch((err) => console.error("MongoDB connection error:", err));7
+
+
+
---
+++ fronted/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold bg-black text-white'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
