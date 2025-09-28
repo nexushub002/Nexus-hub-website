73bf182d1bc5f1d93042cff3f629e51dd98aeddf
meetpatoliya001 <meetpatoliya001@gmail.com>
2025-09-28 15:29:33 +0530

    so

--- fronted/src/pages/Homepage.jsx
+++ fronted/src/pages/Homepage.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import ProductList from '../components/ProductList'
-<<<<<<< HEAD
 import SlideBar from '../components/SlideBar'
-=======
 import CategoryList from '../components/CategoryList'
->>>>>>> 3d0671b4c6a8caf8928159bbdefd652890dff24b
 
 
 
@@ -13,11 +10,8 @@ const Homepage = () => {
   return (
     <div>
       <Navbar/>
-<<<<<<< HEAD
       <SlideBar/>
-=======
       <CategoryList/>
->>>>>>> 3d0671b4c6a8caf8928159bbdefd652890dff24b
       <ProductList/>
     </div>
   )
